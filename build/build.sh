22a418ba547a20798b1091f3a44dae3e09ceb264
118548920+kskshaf@users.noreply.github.com <118548920+kskshaf@users.noreply.github.com>
2025-07-16 17:45:54 +0800

    fix path

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-cc -Wall -Wextra -O3 -static -o im1281b_home *.c -I/usr/local/include -L/usr/local/lib -lserialport -lpthread -lm
+cc -Wall -Wextra -O3 -static -o im1281b_home ../*.c -I/usr/local/include -L/usr/local/lib -lserialport -lpthread -lm
